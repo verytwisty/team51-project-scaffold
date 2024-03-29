3fe6e812700ff9af7c5057f6ccabded0b1088282
Antonius Hegyes <tony.hegyes@automattic.com>
2024-03-29 15:06:02 +0100

    Fix mu-loader references

--- mu-plugins/mu-loader.php
+++ mu-plugins/mu-loader.php
@@ -1,19 +1,18 @@
-<?php
-/**
- * By default, WordPress will load all files in the mu-plugins directory and ignore all the directories inside.
- *
- * This file is a workaround which will load all the plugins in the mu-plugins directory which have a root-level file
- * containing a plugin header (@link https://developer.wordpress.org/plugins/plugin-basics/header-requirements/).
- */
+<?php declare( strict_types=1 );
 
 defined( 'ABSPATH' ) || exit;
 
+/**
+ * By default, WordPress will load all files in the mu-plugins directory and ignore all the directories inside.
+ *
+ * This file is a workaround for loading all the plugins in the mu-plugins directory which have a root-level file
+ * containing a plugin header (@link https://developer.wordpress.org/plugins/plugin-basics/header-requirements/).
+ */
 require_once ABSPATH . 'wp-admin/includes/plugin.php';
-
-foreach ( glob( WPMU_PLUGIN_DIR . '/*/*.php' ) as $bpd_mu_plugin_file ) {
-	$bpd_mu_plugin_data = get_plugin_data( $bpd_mu_plugin_file, false, false );
-	if ( ! empty( $bpd_mu_plugin_data['Name'] ) ) {
-		require_once $bpd_mu_plugin_file;
+foreach ( glob( WPMU_PLUGIN_DIR . '/*/*.php' ) as $wpcomsp_mu_plugin_file ) {
+	$wpcomsp_mu_plugin_data = get_plugin_data( $wpcomsp_mu_plugin_file, false, false );
+	if ( ! empty( $wpcomsp_mu_plugin_data['Name'] ) ) {
+		require_once $wpcomsp_mu_plugin_file;
 	}
 }
 
@@ -29,10 +28,10 @@ add_filter(
 	static function ( array $plugins ): array {
 		unset( $plugins['mustuse']['mu-loader.php'] ); // Remove this file from the list.
 
-		foreach ( glob( WPMU_PLUGIN_DIR . '/*/*.php' ) as $file ) {
-			$plugin_data = get_plugin_data( $file, false, false );
-			if ( ! empty( $plugin_data['Name'] ) ) {
-				$plugins['mustuse'][ basename( $file ) ] = $plugin_data;
+		foreach ( glob( WPMU_PLUGIN_DIR . '/*/*.php' ) as $wpcomsp_mu_plugin_file ) {
+			$wpcomsp_mu_plugin_data = get_plugin_data( $wpcomsp_mu_plugin_file, false, false );
+			if ( ! empty( $wpcomsp_mu_plugin_data['Name'] ) ) {
+				$plugins['mustuse'][ basename( $wpcomsp_mu_plugin_file ) ] = $wpcomsp_mu_plugin_data;
 			}
 		}
 
